0cf97faffab8c1fccd5f3f8d9c349a98d8610fec
tejveersingh78 <tejveer.singh7830@outlook.com>
2022-01-27 22:33:01 +0530

    commited

--- src/main/java/com/sunglowsys/test/FindAll.java
+++ src/main/java/com/sunglowsys/test/FindAll.java
@@ -1,15 +1,17 @@
 package com.sunglowsys.test;
 
 import com.sunglowsys.domain.Customer;
+import com.sunglowsys.domain.Orders;
 import com.sunglowsys.repository.CustomerRepositoryImpl;
+import com.sunglowsys.repository.OrdersRepositoryImpl;
 
 import java.util.List;
 
 public class FindAll {
     public static void main(String[] args) {
-        List<Customer> customerList = new CustomerRepositoryImpl().findAll();
-        for (Customer customer : customerList){
-            System.out.println(customer);
+        List<Orders> ordersList = new OrdersRepositoryImpl().findAll();
+             for (Orders orders : ordersList){
+            System.out.println(orders);
         }
     }
 }
